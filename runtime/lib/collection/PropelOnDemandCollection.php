2f3c2a6b6eb682d21b5f1da72820bc8acf0ec0f1
FΞLIX <felix@celerypayroll.com>
2022-02-15 09:27:41 -0400

    refactor: add fixes for PHP 8.0

--- runtime/lib/collection/PropelOnDemandCollection.php
+++ runtime/lib/collection/PropelOnDemandCollection.php
@@ -155,7 +155,7 @@ class PropelOnDemandCollection extends PropelCollection
         throw new PropelException('The On Demand Collection is read only');
     }
 
-    public function asort()
+    public function asort(int $flags = SORT_REGULAR): bool
     {
         throw new PropelException('The On Demand Collection is read only');
     }
@@ -175,7 +175,7 @@ class PropelOnDemandCollection extends PropelCollection
         throw new PropelException('The On Demand Collection does not allow acces by offset');
     }
 
-    public function ksort()
+    public function ksort(int $flags = SORT_REGULAR): bool
     {
         throw new PropelException('The On Demand Collection is read only');
     }
